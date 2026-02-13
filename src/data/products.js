0e0faaf52f30f9ba3b50632f64d9a587348dd22f
Ezequiel Sanchez <sancheze0114@gmail.com>
2026-02-12 23:39:58 -0300

    archivos de preentrega2 con imagenes

--- src/data/products.js
+++ src/data/products.js
@@ -6,7 +6,7 @@ export const products = [
     category: "herramientas",
     description: "Taladro inalámbrico ideal para trabajos hogareños.",
     stock: 10,
-    image: "public/photo-1622044939413-0b829c342434.avif",
+    image: "/photo-1622044939413-0b829c342434.avif",
   },
   {
     id: "2",
@@ -15,7 +15,7 @@ export const products = [
     category: "herramientas",
     description: "Amoladora potente para corte y desbaste.",
     stock: 5,
-    image: "public/premium_photo-1678454429013-5ef24e6bd739.avif",
+    image: "/premium_photo-1678454429013-5ef24e6bd739.avif",
   },
   {
     id: "3",
@@ -24,7 +24,7 @@ export const products = [
     category: "seguridad",
     description: "Guantes reforzados para trabajo pesado.",
     stock: 25,
-    image: "public/istockphoto-1789042510-2048x2048.jpg",
+    image: "/istockphoto-1789042510-2048x2048.jpg",
   },
   {
     id: "4",
@@ -33,7 +33,7 @@ export const products = [
     category: "seguridad",
     description: "Lentes transparentes contra polvo y partículas.",
     stock: 30,
-    image: "public/istockphoto-91779366-2048x2048.jpg",
+    image: "/istockphoto-91779366-2048x2048.jpg",
   },
   {
     id: "5",
@@ -42,7 +42,7 @@ export const products = [
     category: "accesorios",
     description: "Estuches para organizar herramientas y accesorios.",
     stock: 20,
-    image: "public/istockphoto-2197745882-2048x2048.jpg",
+    image: "/istockphoto-2197745882-2048x2048.jpg",
   },
 ];
 
